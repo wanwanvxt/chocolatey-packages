ebb22598205a5a956dcd8e07283dbc347763e4ee
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2025-05-20 23:32:33 +0700

    use original URLs for downloads FDM and remove local setup files

--- FreeDownloadManager/tools/chocolateyInstall.ps1
+++ FreeDownloadManager/tools/chocolateyInstall.ps1
@@ -1,23 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$file = Join-Path $toolsDir 'fdm_x86_setup.exe'
-$file64 = Join-Path $toolsDir 'fdm_x64_setup.exe'
-
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = $file
-  file64         = $file64
+  url            = 'https://files2.freedownloadmanager.org/6/latest/fdm_x86_setup.exe'
+  url64          = 'https://files2.freedownloadmanager.org/6/latest/fdm_x64_setup.exe'
   softwareName   = 'Free Download Manager'
   checksum       = 'aca9718dc2d77224dcf9f8e530162d51150865d49e5a2b1b7da1bee515dc2eed'
   checksumType   = 'sha256'
-  checksum64     = '3e017e3b6efec76c0716afaa673745682d038cba81bb768cc8a65e05bd911361'
+  checksum64     = '9f59595d7d79650e1fd9ee86c3e115e4ae336e14596f11f6e3a7807135a55170'
   checksumType64 = 'sha256'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
   validExitCodes = @(0, 3010, 1641)
 }
 
-Install-ChocolateyPackage @packageArgs -SkipShim
-Remove-Item -Path $file -ErrorAction Ignore -Force
-Remove-Item -Path $file64 -ErrorAction Ignore -Force
+Install-ChocolateyPackage @packageArgs
